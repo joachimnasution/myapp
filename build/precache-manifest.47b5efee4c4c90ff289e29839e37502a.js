f2d32879cd266d64c986c3204c9268007de9d092
Joachim Nasution <108829445+joachimnasution@users.noreply.github.com>
2022-08-30 21:41:21 +0700

    Add files via upload

---
+++ build/precache-manifest.47b5efee4c4c90ff289e29839e37502a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b54bf16a8a6682c5fb53391c416bbb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22de966e2e5f6f6e3f7",
+    "url": "/static/css/2.58337e42.chunk.css"
+  },
+  {
+    "revision": "29ce7b52b8fd28b1016d",
+    "url": "/static/css/main.441d783a.chunk.css"
+  },
+  {
+    "revision": "e22de966e2e5f6f6e3f7",
+    "url": "/static/js/2.30db3d87.chunk.js"
+  },
+  {
+    "revision": "29ce7b52b8fd28b1016d",
+    "url": "/static/js/main.ec6144af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
